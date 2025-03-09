32839e0a1239f025f1065c071e6a5651b558b1a4
David Y. Stephenson <david@davidystephenson.com>
2025-03-09 12:57:38 -0400

    User service

---
+++ app/service/userService.js
@@ -0,0 +1,26 @@
+const users = require('../data/users.json')
+
+class UserService {
+  constructor () {
+    this.users = users
+  }
+
+  getAll () {
+    return this.users
+  }
+
+  getUserByUsername (username) {
+    const user = this.users.find(user => user.username === username)
+    return user
+  }
+
+  login (username, password) {
+    const user = this.getUserByUsername(username)
+
+    if (!user) {
+      return false
+    }
+
+    return user.password === password
+  }
+}
